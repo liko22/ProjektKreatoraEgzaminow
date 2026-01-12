154b3320d7292918fbb426a3f65f33f81617fc30
Natalia <nmordarska@student.agh.edu.pl>
2026-01-12 19:11:53 +0100

    podstawowe klasy

--- KreatorEgzaminow/src/main/java/KreatorEgzaminow/Egzamin.java
+++ KreatorEgzaminow/src/main/java/KreatorEgzaminow/Egzamin.java
@@ -1,4 +1,26 @@
 package KreatorEgzaminow;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Egzamin {
+    private String nazwaegzaminu;
+    private List<Pytanie> listapytan;
+
+    public Egzamin(String nazwaegzaminu) {
+        this.nazwaegzaminu = nazwaegzaminu;
+        this.listapytan = new ArrayList<>();
+    }
+
+    public void dodajPytanie(Pytanie p) {
+        this.listapytan.add(p);
+    }
+
+    public String getNazwaegzaminu() {
+        return nazwaegzaminu;
+    }
+
+    public List<Pytanie> getListapytan() {
+        return listapytan;
+    }
 }
--- KreatorEgzaminow/src/main/java/KreatorEgzaminow/PytanieJednaOdp.java
+++ KreatorEgzaminow/src/main/java/KreatorEgzaminow/PytanieJednaOdp.java
@@ -1,4 +1,14 @@
 package KreatorEgzaminow;
 
-public class PytanieJednaOdp {
+public class PytanieJednaOdp extends Pytanie {
+    private String poprawnaodpowiedz;
+
+    public PytanieJednaOdp(String tresc, String a, String b, String c, String d, String poprawnaodpowiedz) {
+        super(tresc, a, b, c, d);
+        this.poprawnaodpowiedz = poprawnaodpowiedz;
+    }
+
+    public String getPoprawnaodpowiedz() {
+        return poprawnaodpowiedz;
+    }
 }
