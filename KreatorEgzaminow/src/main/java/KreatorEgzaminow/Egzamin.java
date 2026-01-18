9370c02de7a9e5e7e42353d1c6bca7c9082e449e
GrzegorzLatka <g.latka22@gmail.com>
2026-01-18 04:46:53 +0100

    Logika dzialania egzaminu i obliczania wyniku

--- KreatorEgzaminow/src/main/java/KreatorEgzaminow/Egzamin.java
+++ KreatorEgzaminow/src/main/java/KreatorEgzaminow/Egzamin.java
@@ -1,17 +1,20 @@
 package KreatorEgzaminow;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Egzamin {
-    private List<Pytanie> listaPytanEgzaminacyjnych;
+    private List<Pytanie> listaPytanEgzaminacyjnych = new ArrayList<>();
 
-    public Egzamin() {
-        this.listaPytanEgzaminacyjnych = new ArrayList<>();
-    }
+    public void wylosujPytania(List<Pytanie> baza) {
+        List<Pytanie> kopia = new ArrayList<>(baza);
+        Collections.shuffle(kopia);
 
-    public void dodajPytanie(Pytanie p) {
-        listaPytanEgzaminacyjnych.add(p);
+        listaPytanEgzaminacyjnych.clear();
+        for (int i = 0; i < 10; i++) {
+            listaPytanEgzaminacyjnych.add(kopia.get(i));
+        }
     }
 
     public List<Pytanie> getListaPytanEgzaminacyjnych() {
--- KreatorEgzaminow/src/main/java/KreatorEgzaminow/EgzaminController.java
+++ KreatorEgzaminow/src/main/java/KreatorEgzaminow/EgzaminController.java
@@ -1,11 +1,10 @@
 package KreatorEgzaminow;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class EgzaminController {
@@ -13,51 +12,83 @@ public class EgzaminController {
     @FXML
     private VBox kontenerPytan;
 
-    private Egzamin egzamin;
+    private Egzamin egzamin = new Egzamin();
+    private List<HBox> konteneryOdpowiedzi = new ArrayList<>();
 
     public void rozpocznijTest(List<Pytanie> wszystkie) {
-
-        List<Pytanie> wylosowane = losujPytania(wszystkie);
-        inicjalizujEgzamin(wylosowane);
+        egzamin.wylosujPytania(wszystkie);
         wyswietlPytania();
-    }
-
-    private List<Pytanie> losujPytania(List<Pytanie> lista) {
-        List<Pytanie> kopia = new ArrayList<>(lista);
-        Collections.shuffle(kopia);
-        return kopia.subList(0, 10);
-    }
-
-    private void inicjalizujEgzamin(List<Pytanie> wybrane) {
-        this.egzamin = new Egzamin();
-        for (Pytanie p : wybrane) {
-            this.egzamin.dodajPytanie(p);
-        }
     }
 
     private void wyswietlPytania() {
         kontenerPytan.getChildren().clear();
-        int numer = 1;
+        konteneryOdpowiedzi.clear();
 
-        for (Pytanie p : egzamin.getListaPytanEgzaminacyjnych()) {
-            VBox blokPytania = new VBox(5);
+        for (int i = 0; i < egzamin.getListaPytanEgzaminacyjnych().size(); i++) {
+            Pytanie p = egzamin.getListaPytanEgzaminacyjnych().get(i);
+            VBox blok = new VBox(5);
+            blok.setAlignment(javafx.geometry.Pos.CENTER);
 
-            Label tresc = new Label(numer + ". " + p.getTresc());
+            Label tresc = new Label((i + 1) + ". " + p.getTresc());
             tresc.setStyle("-fx-font-weight: bold;");
 
-            HBox liniaOdpowiedzi = new HBox(20);
+            HBox linia = new HBox(15);
+            linia.setAlignment(javafx.geometry.Pos.CENTER);
 
-            Label oA = new Label("A) " + p.getOpcjaa());
-            Label oB = new Label("B) " + p.getOpcjab());
-            Label oC = new Label("C) " + p.getOpcjac());
-            Label oD = new Label("D) " + p.getOpcjad());
+            CheckBox cb1 = new CheckBox(p.getOpcjaa());
+            CheckBox cb2 = new CheckBox(p.getOpcjab());
+            CheckBox cb3 = new CheckBox(p.getOpcjac());
+            CheckBox cb4 = new CheckBox(p.getOpcjad());
 
-            liniaOdpowiedzi.getChildren().addAll(oA, oB, oC, oD);
+            if (p instanceof PytanieJednaOdp) {
+                CheckBox[] wszystkieCB = {cb1, cb2, cb3, cb4};
+                for (CheckBox cb : wszystkieCB) {
+                    cb.setOnAction(e -> {
+                        if (cb.isSelected()) {
+                            for (CheckBox inne : wszystkieCB) if (inne != cb) inne.setSelected(false);
+                        }
+                    });
+                }
+            }
 
-            blokPytania.getChildren().addAll(tresc, liniaOdpowiedzi);
-            kontenerPytan.getChildren().add(blokPytania);
-
-            numer++;
+            linia.getChildren().addAll(cb1, cb2, cb3, cb4);
+            konteneryOdpowiedzi.add(linia);
+            blok.getChildren().addAll(tresc, linia);
+            kontenerPytan.getChildren().add(blok);
         }
+
+        Button przyciskWynik = new Button("Sprawdź wynik");
+        przyciskWynik.setOnAction(e -> pokazWynik());
+        kontenerPytan.getChildren().add(przyciskWynik);
+    }
+
+    private void pokazWynik() {
+        int punkty = 0;
+
+        for (int i = 0; i < egzamin.getListaPytanEgzaminacyjnych().size(); i++) {
+            Pytanie p = egzamin.getListaPytanEgzaminacyjnych().get(i);
+            HBox linia = konteneryOdpowiedzi.get(i);
+
+            List<String> wybrane = new ArrayList<>();
+            for (javafx.scene.Node node : linia.getChildren()) {
+                CheckBox cb = (CheckBox) node;
+                if (cb.isSelected()) wybrane.add(cb.getText());
+            }
+
+            if (p instanceof PytanieJednaOdp) {
+                if (wybrane.size() == 1 && wybrane.get(0).equals(((PytanieJednaOdp) p).getPoprawnaodpowiedz())) {
+                    punkty++;
+                }
+            } else if (p instanceof PytanieWieleOdp) {
+                List<String> poprawne = ((PytanieWieleOdp) p).getPoprawneodpowiedzi();
+                if (wybrane.size() == poprawne.size() && wybrane.containsAll(poprawne)) {
+                    punkty++;
+                }
+            }
+        }
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setContentText("Wynik: " + punkty + "/" + egzamin.getListaPytanEgzaminacyjnych().size());
+        alert.show();
     }
 }
